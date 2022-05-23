226cb32886925eaadaf563dcf42450932667fb71
LucasGabriell97 <lucas.gabriell97@hotmail.com>
2022-05-23 11:29:23 -0300

    add improvement in javascript

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-let question = document.querySelectorAll('.question')
-let questionBold = document.querySelectorAll('.question h3')
-let addAnswer = document.querySelectorAll('.answer')
+const question = document.querySelectorAll('.question')
 
 for (let elem of question) {
-    elem.addEventListener('click', function() {  
-        if (elem.classList.contains('active')) {
-            elem.nextElementSibling.classList.remove('active')
-            elem.firstElementChild.classList.remove('question-bold')
-            elem.lastElementChild.classList.remove('icon-rotate')
-        } else {
+    elem.addEventListener('click', () => {  
+        if (elem.nextElementSibling.classList.contains('active')) {
             elem.nextElementSibling.classList.toggle('active')
             elem.firstElementChild.classList.toggle('question-bold')
             elem.lastElementChild.classList.toggle('icon-rotate')
+        } else {
+            question.forEach(question => {
+                question.nextElementSibling.classList.remove('active')
+                question.firstElementChild.classList.remove('question-bold')
+                question.lastElementChild.classList.remove('icon-rotate')
+            })
+            elem.nextElementSibling.classList.add('active')
+            elem.firstElementChild.classList.add('question-bold')
+            elem.lastElementChild.classList.add('icon-rotate')
+            
         } 
     }
 )}
